ab8971790995567085aa5c7c6a50e4e4d6171d11
CorneliuZaharescu <cz.zoom@gmail.com>
2018-06-28 19:17:56 +0300

    routing

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('myApp', []);
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "templates/jobs.html"
+        })
+        .when("/details", {
+            templateUrl : "templates/job-details.html"
+        });
+});
